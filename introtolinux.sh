0bd372afda331822c1f2a0062dbc3441e12da5a7
mrgooglegeek <ihagy001@odu.edu>
2019-10-30 15:24:36 -0400

    Update introtolinux.sh

--- introtolinux.sh
+++ introtolinux.sh
@@ -1,6 +1,6 @@
 
 sudo systemctl enable ssh
-sudo cd /etc/network/
+cd /etc/network/
 sudo rm interfaces
 sudo wget https://raw.githubusercontent.com/mrgooglegeek/introtolinux/master/interfaces
 sudo /etc/init.d/networking restart
@@ -9,22 +9,22 @@ sudo iptables -A INPUT -p tcp --dport 22 -j ACCEPT
 sudo iptables -A INPUT -p tcp --dport 80 -j ACCEPT
 sudo iptables -I INPUT 1 -i lo -j ACCEPT
 sudo iptables -A INPUT -j DROP
-sudo groupadd -g grad
-sudo groupadd -g ugrad
-sudo groupadd -g staff
+sudo groupadd grad
+sudo groupadd ugrad
+sudo groupadd staff
 sudo useradd -d /home/grad -m user1
 sudo useradd -d /home/ugrad -m user2
-sudo useradd -d /home/staff -m -G {sudo} user3
+sudo useradd -d /home/staff -m -G sudo user3
 sudo usermod -g grad user1
 sudo usermod -g ugrad user2
 sudo usermod -g staff user3
-sudo cd /home
+cd /home
 sudo chgrp grad /grad
 sudo chgrp ugrad /ugrad
 sudo mkdir everyone
 sudo chmod 1755 ./everyone
 
-sudo cd /var/www/html/
+cd /var/www/html/
 sudo wget https://raw.githubusercontent.com/mrgooglegeek/introtolinux/master/config.php
 sudo wget https://raw.githubusercontent.com/mrgooglegeek/introtolinux/master/index.php
 sudo wget https://raw.githubusercontent.com/mrgooglegeek/introtolinux/master/login.php
@@ -32,7 +32,7 @@ sudo wget https://raw.githubusercontent.com/mrgooglegeek/introtolinux/master/log
 sudo wget https://raw.githubusercontent.com/mrgooglegeek/introtolinux/master/register.php
 sudo wget https://raw.githubusercontent.com/mrgooglegeek/introtolinux/master/reset-password.php
 sudo wget https://raw.githubusercontent.com/mrgooglegeek/introtolinux/master/welcome.php
-sudo cd /tmp/
+cd /tmp/
 sudo wget https://raw.githubusercontent.com/mrgooglegeek/introtolinux/master/makesql.sh
 sudo chmod 755 makesql.sh
 sudo ./makesql.sh CS root root
